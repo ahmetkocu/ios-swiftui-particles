c43777db6003492e9fbe0b17815ab18ac8d3d18f
Ahmet Koçu <ahmetkocu@gmail.com>
2021-02-10 13:41:29 +0300

    first commit

---
+++ Sources/SwiftUIParticles/Particle.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  
+//
+//  Created by helloworld on 10.02.2021.
+//
+
+import Foundation
+
+struct Particle {
+    var radius: Float
+    var x: Float
+    var y: Float
+    var vx: Int
+    var vy: Int
+    var alpha: Float
+}
+
+extension Particle : Hashable {
+    public static func == (lhs: Particle, rhs: Particle) -> Bool {
+        return lhs.x == rhs.x
+    }
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(x)
+    }
+}
---
+++ Sources/SwiftUIParticles/ParticlesView.swift
@@ -0,0 +1,113 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by helloworld on 10.02.2021.
+//
+
+import SwiftUI
+
+struct ParticlesView: View {
+    @State private var particles: [Particle] = [Particle]()
+    @State private var hasSurface: Bool = false
+    @State private var hasSetup: Bool = false
+    @State private var path: Path = Path()
+    
+    // Attribute Defaults
+    var _particleCount = 20
+    var _particleMinRadius = 5
+    var _particleMaxRadius = 10
+    var _particlesBackgroundColor = Color.black
+    var _particleColor = Color.white
+    var _particleLineColor = Color.white
+    var _particleLinesEnabled = true
+    
+    let timer = Timer.publish(
+        every: 0.09,       // Second
+            tolerance: 0.1, // Gives tolerance so that SwiftUI makes optimization
+            on: .main,      // Main Thread
+            in: .common     // Common Loop
+        ).autoconnect()
+    
+    var body: some View {
+        ZStack(alignment: .top) {
+            _particlesBackgroundColor
+                .edgesIgnoringSafeArea(.all)
+            
+            ForEach(particles, id: \.self) { item in
+                
+                ForEach(particles, id: \.self) { item2 in
+                    if item.x != item2.x &&
+                        item.y != item2.y {
+                        
+                        let dx = item.x - item2.x
+                        let dy = item.y - item2.y
+                        let dist = sqrt(dx * dx + dy * dy)
+                        
+                        if dist < 120 {
+                            let distRatio = (120 - dist) / 120
+                            let mm = (min(item.alpha, item2.alpha) * distRatio) / 2
+                            
+                            Path { path in
+                                path.move(to: CGPoint(x: CGFloat(item.x), y: CGFloat(item.y)))
+                                path.addLine(to: CGPoint(x: CGFloat(item2.x), y: CGFloat(item2.y)))
+                            }
+                            //.fill(Color.white)
+                            .stroke(Color.white, lineWidth: 2)
+                            .opacity(Double(mm))
+                        }
+                    }
+                }
+                
+                Circle()
+                    .fill(_particleColor)
+                    .frame(width: 7, height: 7)
+                    .position(x: CGFloat(item.x), y: CGFloat(item.y))
+                    .opacity(Double(item.alpha))
+            }
+        }
+        .onAppear {
+            setupParticles()
+        }
+        .onReceive(timer) { (_) in
+            var pp = [Particle]()
+            for item in particles {
+                var _item = item
+                _item.x += Float(_item.vx)
+                _item.y += Float(_item.vy)
+
+                if (item.x < 0) {
+                    _item.x = Float(UIScreen.main.bounds.width)
+                } else if (item.x > Float(UIScreen.main.bounds.width)) {
+                    _item.x = 0.0
+                }
+
+                if (item.y < 0) {
+                    _item.y = Float(UIScreen.main.bounds.height)
+                } else if (item.y > Float(UIScreen.main.bounds.height)) {
+                    _item.y = 0.0
+                }
+                pp.append(_item)
+            }
+            
+            self.particles = pp
+        }
+    }
+    
+    func setupParticles() {
+        if !hasSetup {
+            hasSetup = true
+            self.particles.removeAll()
+            for _ in 0..._particleCount {
+                particles.append(Particle(radius: Float(Int.random(in: _particleMinRadius..._particleMaxRadius)), x: Float(Int.random(in: 0...Int(UIScreen.main.bounds.width))), y: Float(Int.random(in: 0...Int(UIScreen.main.bounds.height))), vx: Int.random(in: -2...2), vy: Int.random(in: -2...2), alpha: Float.random(in: 0...1)))
+            }
+        }
+    }
+
+}
+
+struct ParticlesView_Previews: PreviewProvider {
+    static var previews: some View {
+        ParticlesView(_particleCount: 20, _particleMinRadius: 5, _particleMaxRadius: 10, _particlesBackgroundColor: .black, _particleColor: .white, _particleLineColor: .white, _particleLinesEnabled: true)
+    }
+}
